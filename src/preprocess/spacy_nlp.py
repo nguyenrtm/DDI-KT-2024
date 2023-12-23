9617cbedb2faa397338c367e8a6502aad6b06d76
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2023-12-23 23:02:27 +0700

    web_lg

--- src/preprocess/spacy_nlp.py
+++ src/preprocess/spacy_nlp.py
@@ -1,5 +1,5 @@
 import spacy
 
 class SpacyNLP:
-    def __init__(self, spacy_model="en_core_sci_lg"):
+    def __init__(self, spacy_model="en_core_web_lg"):
         self.nlp = spacy.load(spacy_model)
