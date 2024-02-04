d3b1b629e2b77c08044ec43a822a8738238d81ca
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2024-02-04 20:27:11 +0700

    word embed

--- src/model/word_embedding.py
+++ src/model/word_embedding.py
@@ -3,9 +3,11 @@ import torch
 from utils import get_trimmed_w2v_vectors, load_vocab
 
 class WordEmbedding:
-    def __init__(self, path='./DDI-KT-2024/data/fasttext/fastText_ddi.npz'):
-        self.vectors = get_trimmed_w2v_vectors(path)
-        self.vocab = load_vocab('./DDI-KT-2024/data/fasttext/all_words.txt')
+    def __init__(self, 
+                 fasttext_path='./DDI-KT-2024/data/fasttext/fastText_ddi.npz', 
+                 vocab_path='./DDI-KT-2024/data/fasttext/all_words.txt'):
+        self.vectors = get_trimmed_w2v_vectors(fasttext_path)
+        self.vocab = load_vocab(vocab_path)
 
     def get_word_vector(self, word):
         if word not in self.vocab.keys():
