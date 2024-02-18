2df1e1282201e0a53065b98c2e2146da060e2ae9
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2024-02-18 20:27:59 +0700

    chge reader

--- src/reader/ddi_reader.py
+++ src/reader/ddi_reader.py
@@ -1,6 +1,7 @@
 import os
 import re
 import xmltodict
+from utils import id_find
 
 class DDICorpusXMLReader:
     def __init__(self, path):
@@ -53,3 +54,50 @@ class DDICorpusXMLReader:
                 medline_list.append(file_dict)
         
         return drugbank_list, medline_list
+    
+def convert_all_list(ddi_dictionary):
+    '''
+    Convert every dict to list in ddi_dictionary.
+    '''
+    for doc in ddi_dictionary:
+        doc = doc['document']
+        if isinstance(doc['sentence'], dict):
+            doc['sentence'] = [doc['sentence']]
+        for sentence in doc['sentence']:
+            if 'entity' in sentence.keys() and isinstance(sentence['entity'], dict):
+                sentence['entity'] = [sentence['entity']]
+            if 'pair' in sentence.keys() and isinstance(sentence['pair'], dict):
+                sentence['pair'] = [sentence['pair']]
+    
+    return ddi_dictionary
+
+def get_candidates(ddi_dictionary):
+    '''
+    Get relation pairs from ddi_dictionary.
+    '''
+    all_candidates = list()
+    for document in ddi_dictionary:
+        document = document['document']
+        for s in document['sentence']:
+            if isinstance(s, dict) and 'pair' in s.keys():
+                for pair in s['pair']:
+                    candidate = dict()
+                    _id = pair['@id']
+                    _e1 = pair['@e1']
+                    _e2 = pair['@e2']
+                    _label = pair['@ddi']
+                    candidate['label'] = _label
+                    if _label == 'true':
+                        try:
+                            assert pair['@type'] in ['effect', 'advise', 'mechanism', 'int']
+                            candidate['label'] = pair['@type']
+                        except:
+                            if _id == 'DDI-DrugBank.d236.s29.p0':
+                                candidate['label'] = 'int'
+                    candidate['id'] = _id
+                    candidate['text'] = s['@text']
+                    candidate['e1'] = id_find(s['entity'], _e1)
+                    candidate['e2'] = id_find(s['entity'], _e2)
+                    assert candidate['label'] != 'true'
+                    all_candidates.append(candidate)
+    return all_candidates
--- src/utils.py
+++ src/utils.py
@@ -31,53 +31,6 @@ def id_find(lst, id):
     for element in lst:
         if element['@id'] == id:
             return element
-        
-def convert_all_list(ddi_dictionary):
-    '''
-    Convert every dict to list in ddi_dictionary.
-    '''
-    for doc in ddi_dictionary:
-        doc = doc['document']
-        if isinstance(doc['sentence'], dict):
-            doc['sentence'] = [doc['sentence']]
-        for sentence in doc['sentence']:
-            if 'entity' in sentence.keys() and isinstance(sentence['entity'], dict):
-                sentence['entity'] = [sentence['entity']]
-            if 'pair' in sentence.keys() and isinstance(sentence['pair'], dict):
-                sentence['pair'] = [sentence['pair']]
-    
-    return ddi_dictionary
-
-def get_candidates(ddi_dictionary):
-    '''
-    Get relation pairs from ddi_dictionary.
-    '''
-    all_candidates = list()
-    for document in ddi_dictionary:
-        document = document['document']
-        for s in document['sentence']:
-            if isinstance(s, dict) and 'pair' in s.keys():
-                for pair in s['pair']:
-                    candidate = dict()
-                    _id = pair['@id']
-                    _e1 = pair['@e1']
-                    _e2 = pair['@e2']
-                    _label = pair['@ddi']
-                    candidate['label'] = _label
-                    if _label == 'true':
-                        try:
-                            assert pair['@type'] in ['effect', 'advise', 'mechanism', 'int']
-                            candidate['label'] = pair['@type']
-                        except:
-                            if _id == 'DDI-DrugBank.d236.s29.p0':
-                                candidate['label'] = 'int'
-                    candidate['id'] = _id
-                    candidate['text'] = s['@text']
-                    candidate['e1'] = id_find(s['entity'], _e1)
-                    candidate['e2'] = id_find(s['entity'], _e2)
-                    assert candidate['label'] != 'true'
-                    all_candidates.append(candidate)
-    return all_candidates
 
 def offset_to_idx(text, offset, nlp):
     '''
