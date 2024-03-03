cc011ab3fda607b40342e680fb33fc3ec99b76f4
Nguyen Binh Nguyen <61158419+nguyenrtm@users.noreply.github.com>
2024-03-03 17:49:11 +0000

    padding

--- src/mol/features.py
+++ src/mol/features.py
@@ -52,7 +52,7 @@ def smi_to_pyg(smi):
     edge_index = list(zip(*atom_pairs))
     if edge_index == []:
       edge_index = torch.LongTensor([(0, 0), (0, 0)])
-      edge_attr = torch.FloatTensor([[0., 0., 0., 0.]])
+      edge_attr = torch.FloatTensor([[0, 0, 2, 2], [0, 0, 2, 2]])
     else:
       edge_index = torch.LongTensor(edge_index)
       edge_attr = torch.FloatTensor(bond_features)
