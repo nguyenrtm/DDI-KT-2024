831631492272e94a2ee66718660e96a883fa03fb
Nguyen Binh Nguyen <61158419+nguyenrtm@users.noreply.github.com>
2024-03-05 09:02:27 +0000

    smiles

--- ddi_kt_2024/multimodal/model.py
+++ ddi_kt_2024/multimodal/model.py
@@ -137,6 +137,9 @@ class MultimodalModel(torch.nn.Module):
             return x
         elif self.modal == '2':
             text_x = self.text_model(text_x)
+            
+            mol_x1_smiles = mol_x1_smiles.squeeze(dim=1).float()
+            mol_x2_smiles = mol_x2_smiles.squeeze(dim=1).float()
 
             x = torch.cat((text_x, mol_x1_smiles, mol_x2_smiles), dim=1)
 
@@ -149,7 +152,10 @@ class MultimodalModel(torch.nn.Module):
             text_x = self.text_model(text_x)
             mol_x1 = self.gnn1(mol_x1)
             mol_x2 = self.gnn2(mol_x2)
-
+            
+            mol_x1_smiles = mol_x1_smiles.squeeze(dim=1).float()
+            mol_x2_smiles = mol_x2_smiles.squeeze(dim=1).float()
+            
             x = torch.cat((text_x, mol_x1, mol_x2, mol_x1_smiles, mol_x2_smiles), dim=1)
 
             # Classifier
