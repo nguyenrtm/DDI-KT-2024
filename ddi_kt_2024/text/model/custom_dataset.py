47a9f2b48fca43c576d2284f77654523f6cc358f
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2024-03-11 01:25:38 +0700

    chg for bc5

--- ddi_kt_2024/text/model/custom_dataset.py
+++ ddi_kt_2024/text/model/custom_dataset.py
@@ -20,33 +20,65 @@ class CustomDataset(Dataset):
                 self.data[i] = torch.zeros((1, 1, 14), dtype=int)
             else:
                 i += 1
+    
+    def rm_none(self):
+        i = 0
+        while i < len(self.data):
+            if self.data[i] == None:
+                self.data[i] = torch.zeros((1, 14))
+            else:
+                i += 1
                 
     def squeeze(self):
         for i in range(len(self.data)):
             self.data[i] = self.data[i].squeeze()
 
-    def batch_padding(self, batch_size, min_batch_size=3):
-        current = 0
-        to_return = []
+    def batch_padding(self, batch_size, min_batch_size=3, dataset='ddi'):
+        if dataset == 'ddi':
+            current = 0
+            to_return = []
 
-        while current + batch_size < len(self.data):
-            batch = self.data[current:current+batch_size]
-            max_len_in_batch = max(max([x.shape[1] for x in batch]), min_batch_size)
+            while current + batch_size < len(self.data):
+                batch = self.data[current:current+batch_size]
+                max_len_in_batch = max(max([x.shape[1] for x in batch]), min_batch_size)
+
+                for i in range(len(batch)):
+                    tmp = F.pad(batch[i], (0, 0, 0, max_len_in_batch - batch[i].shape[1], 0, 0), "constant", 0)
+                    to_return.append(tmp)
+
+                current += batch_size
+
+            batch = self.data[current:]
+            max_len_in_batch = max(max([x.shape[0] for x in batch]), min_batch_size)
 
             for i in range(len(batch)):
                 tmp = F.pad(batch[i], (0, 0, 0, max_len_in_batch - batch[i].shape[1], 0, 0), "constant", 0)
                 to_return.append(tmp)
 
-            current += batch_size
+            self.data = to_return
+        elif dataset == 'bc5':
+            current = 0
+            to_return = []
 
-        batch = self.data[current:]
-        max_len_in_batch = max(max([x.shape[0] for x in batch]), min_batch_size)
+            while current + batch_size < len(self.data):
+                batch = self.data[current:current+batch_size]
 
-        for i in range(len(batch)):
-            tmp = F.pad(batch[i], (0, 0, 0, max_len_in_batch - batch[i].shape[1], 0, 0), "constant", 0)
-            to_return.append(tmp)
+                max_len_in_batch = max(max([x.shape[0] for x in batch]), min_batch_size)
 
-        self.data = to_return
+                for i in range(len(batch)):
+                    tmp = F.pad(batch[i], (0, 0, 0, max_len_in_batch - batch[i].shape[0]), "constant", 0)
+                    to_return.append(tmp)
+
+                current += batch_size
+
+            batch = self.data[current:]
+            max_len_in_batch = max(max([x.shape[0] for x in batch]), min_batch_size)
+
+            for i in range(len(batch)):
+                tmp = F.pad(batch[i], (0, 0, 0, max_len_in_batch - batch[i].shape[0]), "constant", 0)
+                to_return.append(tmp)
+
+            self.data = to_return
 
     def rm_no_smiles(self, list_index):
         new_x = list()
