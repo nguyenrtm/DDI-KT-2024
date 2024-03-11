3a96bb20c59a89c842366a8636352243b61732ea
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2024-03-11 19:05:44 +0700

    Update quick_download.py

--- quick_download.py
+++ quick_download.py
@@ -32,6 +32,101 @@ def download_public_file(bucket_name, source_blob_name, destination_file_name):
         )
     )
 
+def move_blob(bucket_name, blob_name, destination_bucket_name, destination_blob_name,):
+    """Moves a blob from one bucket to another with a new name."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The ID of your GCS object
+    # blob_name = "your-object-name"
+    # The ID of the bucket to move the object to
+    # destination_bucket_name = "destination-bucket-name"
+    # The ID of your new GCS object (optional)
+    # destination_blob_name = "destination-object-name"
+
+    storage_client = storage.Client.create_anonymous_client()
+    source_bucket = storage_client.get_bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    # There is also an `if_source_generation_match` parameter, which is not used in this example.
+    destination_generation_match_precondition = 0
+
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name, if_generation_match=destination_generation_match_precondition,
+    )
+    source_bucket.delete_blob(blob_name)
+
+    print(
+        "Blob {} in bucket {} moved to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name,
+        )
+    )
+
+def copy_blob(
+    bucket_name, blob_name, destination_bucket_name, destination_blob_name,
+):
+    """Copies a blob from one bucket to another with a new name."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+    # destination_bucket_name = "destination-bucket-name"
+    # destination_blob_name = "destination-object-name"
+
+    storage_client = storage.Client.create_anonymous_client()
+    source_bucket = storage_client.get_bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to copy is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    # There is also an `if_source_generation_match` parameter, which is not used in this example.
+    destination_generation_match_precondition = 0
+
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name, if_generation_match=destination_generation_match_precondition,
+    )
+
+    print(
+        "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name,
+        )
+    )
+
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+
+    storage_client = storage.Client.create_anonymous_client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    generation_match_precondition = None
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to delete is aborted if the object's
+    # generation number does not match your precondition.
+    blob.reload()  # Fetch blob metadata to use in generation_match_precondition.
+    generation_match_precondition = blob.generation
+
+    blob.delete(if_generation_match=generation_match_precondition)
+
+    print(f"Blob {blob_name} deleted.")
+
 @click.command()
 @click.option("-o", "--option", required=True, type=str, help="download/upload option only")
 @click.option("--bucket_path", required=True, type=str, help="Example: ddi_kt_2024_multimodal_research/no_padding_customdataset/biobert_cased_mean/train.pt")
