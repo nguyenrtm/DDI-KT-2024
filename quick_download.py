5d5a20c4aab24f5b4950c6341dc3ef75244c6f8b
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2024-03-11 19:01:03 +0700

    Update quick_download.py

--- quick_download.py
+++ quick_download.py
@@ -8,8 +8,8 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket. https://cloud.google.com/storage/docs/ """
     storage_client = storage.Client.create_anonymous_client()
     bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_name)
+    blob = bucket.blob(source_file_name)
+    blob.upload_from_filename(destination_blob_name)
     print('File {} uploaded to {}.'.format(
         source_file_name,
         destination_blob_name))
