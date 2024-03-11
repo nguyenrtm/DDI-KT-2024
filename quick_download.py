279fc2d94e20854bc625d93f2726b5fdd2541cc7
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2024-03-11 15:19:20 +0000

    fix quick download

--- quick_download.py
+++ quick_download.py
@@ -35,14 +35,37 @@ def download_public_file(bucket_name, source_blob_name, destination_file_name):
 @click.command()
 @click.option("-o", "--option", required=True, type=str, help="download/upload option only")
 @click.option("--bucket_path", required=True, type=str, help="Example: ddi_kt_2024_multimodal_research/no_padding_customdataset/biobert_cased_mean/train.pt")
-@click.option("--local_path", required=True, type=str, help="Example: /kaggle/working/DDI-KT-2024/train.pt")
-def process(option, bucket_path, local_path):
+@click.option("--local_path", type=str, default=None, help="Example: /kaggle/working/DDI-KT-2024/train.pt")
+@click.option("--bucket_dest", type=str, default=None, help="For move and copy operation")
+def process(option, bucket_path, local_path, bucket_dest):
     bucket_name = bucket_path.split("/")[0]
     source_blob_name = "/".join(bucket_path.split("/")[1:])
     if option == "download":
+        if local_path is None:
+            print("Please give the local path")
+            return
         download_public_file(bucket_name, source_blob_name, local_path)
     if option == "upload":
-        upload_blob(bucket_name, source_file_name, local_path)
+        if local_path is None:
+            print("Please give the local path")
+            return
+        upload_blob(bucket_name, source_blob_name, local_path)
+    if option == "move":
+        if bucket_dest is None:
+            print("Please give the bucket dest")
+            return
+        dest_name = bucket_dest.split("/")[0]
+        dest_blob_name = "/".join(bucket_dest.split("/")[1:])
+        move_blob(bucket_name, source_blob_name, dest_name, dest_blob_name)
+    if option == 'copy':
+        if bucket_dest is None:
+            print("Please give the bucket dest")
+            return
+        dest_name = bucket_dest.split("/")[0]
+        dest_blob_name = "/".join(bucket_dest.split("/")[1:])
+        copy_blob(bucket_name, source_blob_name, dest_name, dest_blob_name)
+    if option == "delete":
+        delete_blob(bucket_name, source_blob_name)
 
 if __name__=="__main__":
     process()
