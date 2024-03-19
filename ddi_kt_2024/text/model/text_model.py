b096743d57aebb7ebcdb216db24f034f19f49068
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2024-03-19 22:08:28 +0700

    multi head attn

--- ddi_kt_2024/text/model/text_model.py
+++ ddi_kt_2024/text/model/text_model.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 
-from bc5_2024.text.model.self_attention import SelfAttention
-
 class TextModel(nn.Module):
     def __init__(self,
                  we,
@@ -114,7 +112,6 @@ class TextModel(nn.Module):
                                         out_features=target_class,
                                         bias=False)
 
-        self.self_attention = SelfAttention(input_dim=token_embedding_size * 2 + dep_embedding_size)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
@@ -186,8 +183,6 @@ class TextModel(nn.Module):
         dep = self.dropout(self.relu(self.normalize_dep(dep)))
 
         x = torch.cat((tokens_ent1, dep, tokens_ent2), dim=2)
-
-        x = self.self_attention(x)
 
         if self.model_option == 'cnn':
             x = x.unsqueeze(1)
@@ -322,7 +317,10 @@ class BertModel(nn.Module):
                                           out_features=target_class,
                                           bias=False)
             
-        self.self_attention = SelfAttention(input_dim=token_embedding_size * 2 + dep_embedding_size)
+        self.self_attention=nn.MultiheadAttention(embed_dim=token_embedding_size*2+dep_embedding_size, 
+                                            num_heads=8, 
+                                            dropout=dropout_rate,
+                                            batch_first=True)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
@@ -396,7 +394,7 @@ class BertModel(nn.Module):
         
         x = torch.cat((tokens_ent1, dep, tokens_ent2), dim=2)
     
-        x = self.self_attention(x)
+        x, attention_weights_output = self.self_attention(x, x, x)
 
         if self.model_option == 'cnn':
             x = x.unsqueeze(1)
