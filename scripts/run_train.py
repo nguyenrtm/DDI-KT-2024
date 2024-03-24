0f25abd997c4689d1d26da4577b64f34b29147e9
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2024-03-24 10:45:00 +0000

    fix

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -96,7 +96,9 @@ def run_train(yaml_path):
         data_test = torch.load(config.test_custom_dataset)
         if hasattr(data_train, "random_sampler"):
             train_sampler = RandomSampler(data_train)
-        train_dataloader = DataLoader(data_train, sampler=train_sampler, batch_size=config.batch_size)
+            train_dataloader = DataLoader(data_train, sampler=train_sampler, batch_size=config.batch_size)
+        else:
+            train_dataloader = DataLoader(data_train, batch_size=config.batch_size)
         test_dataloader = DataLoader(data_test, batch_size=config.batch_size)
     else:
         raise ValueError("Value of type_embed isn't supported yet!")
